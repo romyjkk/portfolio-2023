8ffefbc83568a3212be0765454e762f7d5b903a9
romyjkk <98404201+romyjkk@users.noreply.github.com>
2023-11-16 17:44:15 +0100

    finished the homepage

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const openMenuButton = document.getElementById("open-menu");
+const closeMenuButton = document.getElementById("close-menu");
+
+function openMenu() {
+  hamburgerMenu.classList.remove("hidden");
+  hamburgerMenu.classList.add("open-menu");
+
+  openMenuButton.classList.add("hidden");
+  closeMenuButton.classList.remove("hidden");
+}
+
+openMenuButton.addEventListener("click", openMenu);
+
+function closeMenu() {
+  hamburgerMenu.classList.remove("open-menu");
+  hamburgerMenu.classList.add("hidden");
+  closeMenuButton.classList.add("hidden");
+  openMenuButton.classList.remove("hidden");
+}
+
+closeMenuButton.addEventListener("click", closeMenu);
